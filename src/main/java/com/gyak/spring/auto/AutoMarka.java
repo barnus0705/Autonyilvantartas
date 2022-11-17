bee13166b5c8e15ccd6bb6770b8d5b4bf34712cd
barna <barnabaskaroly@gmail.com>
2022-11-17 09:42:59 +0100

    done

--- src/main/java/com/gyak/spring/auto/AutoMarka.java
+++ src/main/java/com/gyak/spring/auto/AutoMarka.java
@@ -1,5 +1,5 @@
 package com.gyak.spring.auto;
 
 public enum AutoMarka {
-    Opel,Audi,Bmw,Tavria,Lada,Ford,Lamborgini,Fiat,AlfaRomeo
+    Opel,Audi,Bmw,Tavria,Lada,Ford,Lamborgini,Fiat,AlfaRomeo,Honda
 }
